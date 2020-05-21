ece206b0776a7749a6353fa3d1bfc46d77b607bd
William Koller <wkoller25@gmail.com>
2020-05-21 01:07:01 -0300

    call database config

--- logic/getLessons.ts
+++ logic/getLessons.ts
@@ -1,20 +1,13 @@
-import { Client } from "https://deno.land/x/mysql/mod.ts?"
 import Class from '../model/class.ts'
+import { client } from '../config/database/data.ts'
 
 export default async (): Promise<Class[]> => {
-  const client = await new Client().connect({
-    hostname: '0.0.0.0',
-    username: 'root',
-    password: 'root',
-    db: 'wcloud'
-  })
-  
   const register = await client.query('SELECT id, name FROM lessons')
-  const lessons: Class[] = register.map((reg: any): Class =>{
-    return {
-      id: reg.id,
-      name: reg.name
-    }
-  })
-  return lessons
+    const lessons: Class[] = register.map((reg: any): Class =>{
+      return {
+        id: reg.id,
+        name: reg.name
+      }
+    })
+    return lessons
 }
